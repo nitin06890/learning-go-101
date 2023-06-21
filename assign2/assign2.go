88a32a9f7632035bca08c196cde74a7166c67385
nitin06890 <57109530+nitin06890@users.noreply.github.com>
2023-06-21 10:15:20 +0530

    add assign2

---
+++ assign2/assign2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var integerVar int = 10
+	var floatVar float64 = 20.5
+	var stringVar string = "Hi Shelby7!"
+
+	fmt.Println("Integer Variable = ", integerVar)
+	fmt.Println("Float Variable = ", floatVar)
+	fmt.Println("String Variable = ", stringVar)
+}
