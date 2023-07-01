0228b2ead3110112e104e3a0565a216882471ec1
nitin06890 <57109530+nitin06890@users.noreply.github.com>
2023-07-01 15:48:44 +0530

    add assign24

--- assign24/assign24.go
+++ assign24/assign24.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	file, err := os.OpenFile("testfile.txt", os.O_WRONLY|os.O_CREATE, 700)
+	file, err := os.OpenFile("testfile.txt", os.O_WRONLY|os.O_CREATE, 0700)
 	if err != nil {
 		fmt.Println("Open operation failed")
 		os.Exit(1)
