332cef1f01430c22d573e405f2f0f538af7ba1f9
nitin06890 <57109530+nitin06890@users.noreply.github.com>
2023-07-01 15:47:57 +0530

    add assign24

---
+++ assign24/assign24.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.OpenFile("testfile.txt", os.O_WRONLY|os.O_CREATE, 700)
+	if err != nil {
+		fmt.Println("Open operation failed")
+		os.Exit(1)
+	} else {
+		fmt.Println("Open operation succeeded")
+	}
+	_ = file
+}
